d14a83c518296e83a23bd4150d99533834e09d71
RylaD303 <alemak2002@gmail.com>
2022-12-06 22:06:59 +0200

    Added test for vector creating.

--- tests/Vector2D_test.py
+++ tests/Vector2D_test.py
@@ -5,6 +5,15 @@ import unittest
 class TestingVector(unittest.TestCase):
 
     def test_creating_vectors(self):
+        vector1 = Vector2D(1,-1)
+        self.assertEqual(vector1.x, 1)
+        self.assertEqual(vector1.y, -1)
+        self.assertEqual(vector1.x, vector1.x)
+
+        vector2 = Vector2D(1,-2)
+        self.assertEqual(vector1.x, vector2.x)
+        self.assertNotEqual(vector1.y, vector2.y)
+        self.assertEqual(vector1.y, vector1.y)
 
 if __name__ == '__main__':
     unittest.main()
