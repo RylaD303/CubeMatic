7fe82db31f54cb48c877e657bfb38c343d1ec509
RylaD303 <alemak2002@gmail.com>
2022-12-09 23:53:50 +0200

    Added type hints for GameObject.

--- src/classes/GameObject.py
+++ src/classes/GameObject.py
@@ -5,5 +5,5 @@ import pygame
 
 
 class GameObject():
-    def __init__(self, x: number_types, y: number_types):
+    def __init__(self, x: number_types, y: number_types) -> "GameObject":
         self.position = Vector2D(x,y)
