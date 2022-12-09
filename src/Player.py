266e20a4a89f3c779bf1b8967340b700f591a251
RylaD303 <alemak2002@gmail.com>
2022-12-09 23:53:18 +0200

    Added type hints for player.

--- src/Player.py
+++ src/Player.py
@@ -1,15 +1,15 @@
 from typing import Union
-from classes.Vector2D import Vector2D
+from classes.Vector2D import Vector2D, number_types
 from classes.GameObject import GameObject
 import pygame
 
 
 class Player(GameObject):
-    def __init__(self, health_points, speed, width, height):
+    def __init__(self, health_points : number_types, speed : number_types, width : number_types = 200, height : number_types = 200) -> "Player":
         self.health_points = health_points
         self.speed = speed
         self.width = width
         self.height = height
 
-    def main(self, display):
+    def main(self, display) -> None:
         pygame.draw.rect(display, (0, 255, 0), (self.position.x, self.position.y, self.width, self.height))
