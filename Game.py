879147118cafae488a91f3dc9cf0d81197c3f205
RylaD303 <alemak2002@gmail.com>
2022-12-10 20:57:14 +0200

    Adde 60FPS.

--- Game.py
+++ Game.py
@@ -2,8 +2,8 @@ import pygame, sys
 from pygame.locals import *
 from src import Bullet, Player
 
-
 pygame.init()
+clock = pygame.time.Clock()
 
 WINDOW_SIZE =(400,400)
 
@@ -14,4 +14,7 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+
     pygame.display.update()
+    clock.tick(60)
