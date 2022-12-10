c46eb41da696d3610479b9036823ce1cdfbd121a
RylaD303 <alemak2002@gmail.com>
2022-12-10 23:41:02 +0200

    Added movement to character.

--- Game.py
+++ Game.py
@@ -6,7 +6,11 @@ from src.classes.Vector2D import Vector2D
 
 pygame.init()
 clock = pygame.time.Clock()
-player = Player(Vector2D(20,20))
+
+PLAYER_START = Vector2D(20,20)
+PLAYER_SPEED = 5
+player = Player(PLAYER_START, PLAYER_SPEED)
+player_movement = [False,False,False,False]
 WINDOW_SIZE =(800,400)
 
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
@@ -19,6 +23,26 @@ while True:
             pygame.quit()
             sys.exit()
 
-    player.main(screen)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                player_movement[0] = True
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                player_movement[1] = True
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                player_movement[2] = True
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
+                player_movement[3] = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                player_movement[0] = False
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                player_movement[1] = False
+            if event.key == pygame.K_UP or event.key == ord('w'):
+                player_movement[2] = False
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
+                player_movement[3] = False
+
+    player.main(screen, player_movement)
     pygame.display.update()
     clock.tick(60)
--- src/Player.py
+++ src/Player.py
@@ -5,14 +5,29 @@ import pygame
 
 
 class Player(GameObject):
-    def __init__(self, position : "Vector2D", health_points : number_types = 1,
-    speed : number_types = 5, width : number_types = 20, height : number_types = 20) -> "Player":
+    def __init__(self, position : "Vector2D", speed : number_types = 5,
+    width : number_types = 20, height : number_types = 20) -> "Player":
 
         super().__init__(position)
-        self.health_points = health_points
         self.speed = speed
         self.width = width
         self.height = height
 
-    def main(self, display) -> None:
+    def main(self, display, player_movement : list[bool]) -> None:
+        self.__move(player_movement)
         pygame.draw.rect(display, (0, 255, 0), (self.position.x, self.position.y, self.width, self.height))
+
+    def __move(self, player_movement : list[bool]):
+        movement = Vector2D(0,0)
+        if player_movement[0] :
+            movement.x -= 1
+        if player_movement[1] :
+            movement.x += 1
+        if player_movement[2] :
+            movement.y -= 1
+        if player_movement[3] :
+            movement.y += 1
+
+        self.position += self.speed*movement/abs(movement)
+
+
