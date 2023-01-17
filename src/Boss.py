8ae0b24464283bac72c2986fcdee118221181a34
RylaD303 <alemak2002@gmail.com>
2023-01-18 00:18:21 +0200

    Added evaluation for attack pattern FiveWaveShooter.

--- src/Boss.py
+++ src/Boss.py
@@ -7,6 +7,7 @@ from src.GameValues import *
 from random import sample
 from queue import Queue
 from enum import Enum
+from math import sin, cos, pi
 import pygame
 
 class FollowingAttackPattern(Enum):
@@ -46,18 +47,41 @@ class Boss(GameObject):
         self.movement_pattern = Queue()
         self.current_attack_pattern = None
         self.time_to_execute = 0
+        self.attack_cooldown = 0
 
-    def __choose_attack_sequence(self):
+    def __choose_attack_sequence(self)-> None:
         #for attack in sample(following_attacks, 3):
         #    self.attack_sequence.put(attack)
         self.attack_sequence.put(FollowingAttackPattern.FiveWaveShoot)
 
-    def __evaluate_attack_pattern(self):
+    def __evaluate_attack_pattern(self) -> None:
         if self.time_to_execute <= 0:
             self.current_attack_pattern = self.attack_sequence.get()
             self.time_to_execute = 6000 #ms
 
-    def __execute_attack_pattern(self, player: "Player", boss_bullets: list["Bullet"], clock):
+    def __execute_attack_pattern(self, player: "Player", boss_bullets: list["Bullet"], clock: "pygame.time.Clock") -> None:
+
+        def create_bullet_with_angle(angle: number_types) -> "Bullet":
+            bullet_direction = Vector2D(
+                            player.position.x*cos(angle) - player.position.y*sin(angle),
+                            player.position.x*sin(angle) + player.position.y*cos(angle))
+
+            return Bullet(self.position, bullet_direction)
+
+
         #self.time_to_execute = 6000 #ms
-        
+        self.time_to_execute -= clock.get_time()
+        self.attack_cooldown -= clock.get_time()
+        self.__evaluate_attack_pattern()
+
+        if self.current_attack_pattern == FollowingAttackPattern.FiveWaveShoot:
+            if self.attack_cooldown <=0:
+                self.attack_cooldown = 800 #ms
+                angle = pi/9
+                #central bullet
+                boss_bullets.append(Bullet(self.position, player.position))
+                for i in range(1,3):
+                    boss_bullets.append(create_bullet_with_angle(i*angle))
+                    boss_bullets.append(create_bullet_with_angle(-i*angle))
+
 
