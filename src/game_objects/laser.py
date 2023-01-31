c95dc30da9c3073ba183c7e8d6d555c335820bd4
RylaD303 <alemak2002@gmail.com>
2023-01-31 21:58:42 +0200

    Reworked laser docstrings.

--- src/game_objects/laser.py
+++ src/game_objects/laser.py
@@ -5,6 +5,18 @@ import pygame
 from enum import Enum
 
 WHITE = (255, 255 ,255)
+class LaserState(Enum):
+    """Enum for laser state"""
+    Anticipation = 1
+    Attack = 2
+    Recovery = 3
+
+class LaserMovement(Enum):
+    """Enum to describe the laser movements"""
+    Constant = 1
+    AcceleratingStart = 2
+    DeceleratingEnd = 3
+
 
 class Laser(GameObject):
     """
@@ -12,44 +24,55 @@ class Laser(GameObject):
 
     Handles own movement and chagne of states.
     """
-    class LaserState(Enum):
-        """Enum for laser state"""
-        Anticipation = 1
-        Attack = 2
-        Recovery = 3
-
-    class LaserMovement(Enum):
-        """Enum to describe the laser movements"""
-        Constant = 1
-        AcceleratingStart = 2
-        DeceleratingEnd = 3
-
 
     def __init__(self,
         begin_point: "Vector2D",
         direction: "Vector2D",
         time: number_types, #ms
         width: number_types = 10,
-        color: tuple = (255, 0, 0),
-        laser_rotation_speed: number_types = 0)-> None:
+        color: tuple = (255, 0, 0))-> None:
         """
-        Initialises the Laser.
+        Initialises the Laser object.
 
         Parameters:
+            begin_point -
+                Starting point of the laser.
+            direction -
+                Direction of the laser.
+            time -
+                time for the laser to live.
+            width -
+                width of the laser.
+            color -
+                color with which the laser will be rendered.
         """
-        super().__init__(begin_point) #starting position
+        super().__init__(begin_point)
         self.direction = (direction/abs(direction))*(LASER_LENGTH**2)
         self.width = width
-        self.starting_width = width
+        self.starting_width = 0
         self.color = color
-        self.state = Laser.LaserState.Anticipation
-        self.movement_types = [Laser.LaserMovement.Constant]
-        self.rotation_speed = laser_rotation_speed
+        self.state = LaserState.Anticipation
+        self.movement_types = [LaserMovement.Constant]
+        self.rotation_speed = 0
         self.cooldown = LASER_ANTICIPATION_TIME
         self.time_to_execute = time
 
 
-    def main(self, clock: "pygame.time.Clock", new_position: "Vector2D" = None):
+    def main(self,
+            clock: "pygame.time.Clock",
+            new_position: "Vector2D" = None):
+        """
+        Handles main part of the laser - movement, state evaluation,
+        time left to execute.
+
+        Parameters:
+            clock -
+                to evaluate time left to live.
+            new_position -
+                the new position at frame of which the laser should be
+                comnig from. If not given one, the laser stays in one
+                place.
+        """
         self.cooldown -= clock.get_time()
         self.time_to_execute -= clock.get_time()
         self.direction.angle_rotate(self.rotation_speed*clock.get_time()/1000)
@@ -59,59 +82,116 @@ class Laser(GameObject):
         self.__evaluate_state()
         self.__evaluate_movement(clock)
 
-        if self.state == Laser.LaserState.Recovery:
-            self.width = (self.starting_width*(self.time_to_execute/LASER_ANTICIPATION_TIME))
+        if self.state == LaserState.Recovery:
+            self.width =(self.starting_width\
+                        * (self.time_to_execute/LASER_ANTICIPATION_TIME))
 
     def render(self, display: "pygame.Surface"):
-        if self.state == Laser.LaserState.Anticipation:
+        """
+        Renders the laser on a display.
+        The function renders the laser in a different ways depending
+        on its current state. If the state is anticipation then the
+        laser renders as slightly faded self. If the state is attack,
+        then the laser pritns as it should be, and if it is in recovery,
+        the laser contracts.
+        Parameters:
+            display - pygame.Surface
+                to print the player on the surface.
+        """
+        if self.state == LaserState.Anticipation:
             new_color = (self.color[0]/2, self.color[1]/2, self.color[2]/2)
-            pygame.draw.line(display, new_color, tuple(self.position), tuple(self.direction), self.width)
-        elif self.state == Laser.LaserState.Attack:
-            #pygame.draw.line(display, WHITE, tuple(self.position), tuple(self.direction), round(self.width+self.width*0.5))
-            pygame.draw.line(display, self.color, tuple(self.position), tuple(self.direction), self.width)
+            pygame.draw.line(display,
+                            new_color,
+                            tuple(self.position),
+                            tuple(self.direction),
+                            round(self.width))
+
+        elif self.state == LaserState.Attack:
+            pygame.draw.line(display,
+                            self.color,
+                            tuple(self.position),
+                            tuple(self.direction),
+                            round(self.width))
         else:
-            #pygame.draw.line(display, WHITE, tuple(self.position), tuple(self.direction), round(self.width+self.width*0.5))
-            pygame.draw.line(display, self.color, tuple(self.position), tuple(self.direction), round(self.width))
+            pygame.draw.line(display,
+                            self.color,
+                            tuple(self.position),
+                            tuple(self.direction),
+                            round(self.width))
 
     def __evaluate_state(self):
+        """
+        Evaluates the current statec of the laser.
+        States are in the order:
+        Anticipation,
+        Attack,
+        Recovery.
+        """
         if self.cooldown<=0:
-            if self.state == Laser.LaserState.Anticipation:
-                self.state = Laser.LaserState.Attack
+            if self.state == LaserState.Anticipation:
+                self.state = LaserState.Attack
                 self.cooldown = self.time_to_execute-LASER_ANTICIPATION_TIME
-            elif self.state == Laser.LaserState.Attack:
-                self.state = Laser.LaserState.Recovery
+            elif self.state == LaserState.Attack:
+                self.state = LaserState.Recovery
                 self.time_to_expire = LASER_ANTICIPATION_TIME
                 self.cooldown = LASER_ANTICIPATION_TIME
 
 
     def __evaluate_movement(self, clock: "pygame.time.Clock"):
+        """
+        Evaluates the movement of the laser, given the movement_types
+        it was given.
 
-        if self.movement_types[0] == Laser.LaserMovement.Constant:
+        Parameters:
+            clock -
+                to set cooldowns with.
+        """
+        if self.movement_types[0] == LaserMovement.Constant:
             pass #nothing to do here
 
-        elif self.movement_types[0] == Laser.LaserMovement.AcceleratingStart and self.state == Laser.LaserState.Attack:
+        elif self.movement_types[0] == LaserMovement.AcceleratingStart\
+             and self.state == LaserState.Attack:
+
             if self.rotation_speed < self.max_rotation_speed:
-                self.rotation_speed += (clock.get_time()/1000)*self.control_rotation_speed
+                self.rotation_speed +=\
+                    (clock.get_time()/1000)*self.control_rotation_speed
             else:
                 self.movement_types.pop(0)
 
-        elif self.movement_types[0] == Laser.LaserMovement.DeceleratingEnd and self.state == Laser.LaserState.Attack and self.time_to_execute < LASER_ANTICIPATION_TIME*2:
+        elif self.movement_types[0] == LaserMovement.DeceleratingEnd\
+            and self.state == LaserState.Attack\
+            and self.time_to_execute < LASER_ANTICIPATION_TIME*2:
+
             if self.rotation_speed > self.min_rotation_speed:
-                self.rotation_speed -= (clock.get_time()/1000)*self.control_rotation_speed
+                self.rotation_speed -=\
+                    (clock.get_time()/1000)*self.control_rotation_speed
             else:
                 self.movement_types.pop(0)
 
 
     def set_type(self,
-        move_types: list["Laser.LaserMovement"] = [LaserMovement.Constant],
+        move_types: list["LaserMovement"] = [LaserMovement.Constant],
         control_rotation_speed: number_types = 0, #pi/s
         max_rotation_speed: number_types = 0, #pi/s
         min_rotation_speed: number_types = 0, #pi/s
         )->None:
+        """
+        Sets the type of movement for the laser.
+        All speeds calculated with pi/s.
+        Parameters:
+            move_types -
+                list of laser type movements.
+            control_rotation_speed -
+                at what speed should the laser accelerate/decelerate
+            max_rotation_speed -
+                the max speed the laser can reach.
+            min_rotation_speed -
+                the minimum speed at which the laser is rotating.
+        """
         if not isinstance(move_types, list):
             move_types = [move_types]
-        if move_types[-1] != Laser.LaserMovement.Constant:
-            move_types.append(Laser.LaserMovement.Constant)
+        if move_types[-1] != LaserMovement.Constant:
+            move_types.append(LaserMovement.Constant)
         self.movement_types = move_types
         self.control_rotation_speed = control_rotation_speed
         self.max_rotation_speed = max_rotation_speed
